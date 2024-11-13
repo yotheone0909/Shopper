8e2cc20c20ab31838f383cc3d1f0b9639c2b0cfb
Yothin <yothin.indy.8@gmail.com>
2024-11-13 22:29:06 +0700

    Modified Test

--- presentation/src/main/java/com/example/shopper/ui/feature/summary/CartSummaryViewModel.kt
+++ presentation/src/main/java/com/example/shopper/ui/feature/summary/CartSummaryViewModel.kt
@@ -15,7 +15,6 @@ class CartSummaryViewModel(private val getCartSummaryUseCase: CartSummaryUseCase
     val uiState = _uiState.asStateFlow()
 
     init {
-
     }
 
     fun getCartSummary(userId: Int) {
